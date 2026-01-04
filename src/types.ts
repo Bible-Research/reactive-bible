82d76d6d67ad5425ce532646d315be0a0e1b1664
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2026-01-04 07:21:23 +0200

    Feat: Define Note and Tag types

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface Tag {
+  id: string;
+  name: string;
+  parent_tag: string | null;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Note {
+  id: string;
+  note_text: string;
+  public: boolean;
+  created_at: string;
+  updated_at: string;
+  tag: Tag;
+  verses: {
+    book: string;
+    chapter: number;
+    verse: number;
+    text: string;
+  }[];
+}
