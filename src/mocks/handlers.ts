95455f283bb1eb2ecdd3fd10eeb1288982ae2ef9
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2026-01-26 03:24:22 +0200

    feat(tests): add msw for api mocking

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,9 @@
+import { http, HttpResponse } from 'msw';
+
+// Define your request handlers
+export const handlers = [
+  // Example handler:
+  // http.get('/api/user', () => {
+  //   return HttpResponse.json({ firstName: 'John', lastName: 'Maverick' })
+  // })
+];
